d7451bd77d025f7133725800d34e71ba39179246
Simone <simonestranieri2518@gmail.com>
2022-12-13 12:34:47 +0100

    More lessons!!
    - Auto-generated files from PyCharm
    
    - Three other lessons

---
+++  FourthWave/mutations.py
@@ -0,0 +1,9 @@
+def mutate_string(string, position, character):
+    out = [i for i in string]
+    out[position] = character
+    return "".join(i for i in out)
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
