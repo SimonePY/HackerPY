8b94e10765c7b0c3661d95255881a365b1b222af
Simone <simonestranieri2518@gmail.com>
2023-01-30 12:10:56 +0100

    Completed another wave

---
+++ EighthWave/ordDict.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import OrderedDict
+d = OrderedDict()
+for _ in range(int(input())):
+    item, space, quantity = input().rpartition(' ')
+    d[item] = d.get(item, 0) + int(quantity)
+for item, quantity in d.items():
+    print(item, quantity)
