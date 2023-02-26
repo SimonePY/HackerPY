c187d93ed1fa70b760d746ddce67130dce685b32
SimonePY <92288879+SimonePY@users.noreply.github.com>
2023-02-26 21:28:01 +0100

    New lessons completed

---
+++ TenthWave/compress.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import groupby
+for k, c in groupby(input()):
+ print("(%d, %d)" % (len(list(c)), int(k)), end=' ')
