7def3aaa0948ee88e097393c4d8dc9c946d1bc4e
SimonePY <92288879+SimonePY@users.noreply.github.com>
2023-04-01 13:13:10 +0200

    Wave Completed

---
+++ FifteenthWave/athSort.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+
+    P = sorted(arr, key=lambda row: row[k])
+    for i in range(len(P)):
+        for j in range(len(P[i])):
+            print(P[i][j], end=' ')
+        print()
