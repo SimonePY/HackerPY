6561f255d7323784a4b5fa7581f0e3e67b1ba272
SimonePY <92288879+SimonePY@users.noreply.github.com>
2023-04-13 01:58:11 +0200

    Another wave is completed

---
+++ SixteenthWave/detectFloating.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+
+class Main():
+    def __init__(self):
+        self.n = int(input())
+
+        for i in range(self.n):
+            self.s = input()
+            print(bool(re.match(r'^[-+]?[0-9]*\.[0-9]+$', self.s)))
+
+
+if __name__ == '__main__':
+    obj = Main()
